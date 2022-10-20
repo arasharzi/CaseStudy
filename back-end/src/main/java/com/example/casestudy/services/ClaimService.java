f0f07487f1bdb509c842d11b169942d416761595
Arash Arzi <arasharzi@gmail.com>
2022-10-20 15:57:10 -0400

    initial back-end

--- back-end/src/main/java/com/example/casestudy/services/ClaimService.java
+++
@@ -1,6 +0,0 @@
-package com.example.casestudy.services;
-
-public class ClaimService
-{
-    // TODO: claim service
-}
--- back-end/src/main/java/com/example/casestudy/services/IClaimService.java
+++
@@ -1,6 +0,0 @@
-package com.example.casestudy.services;
-
-public interface IClaimService
-{
-    // TODO: claim service interface
-}
