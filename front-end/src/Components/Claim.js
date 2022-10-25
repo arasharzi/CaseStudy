d1cc7b720972660f6a8817bdbee1317a4a2487ef
Arash Arzi <arasharzi@gmail.com>
2022-10-25 14:41:54 -0400

    completed landing page, implemented claim page shell

--- front-end/src/Components/Claim.js
+++ front-end/src/Components/Claim.js
@@ -1,14 +1,67 @@
 import React, { useState } from "react"
+import { MDBTabs, MDBTabsItem, MDBTabsLink, MDBTabsContent, MDBTabsPane, MDBRow, MDBCol } from 'mdb-react-ui-kit'
 
 
 function Claim ()
 {   
+    const [verticalActive, setVerticalActive] = useState('tab1');
 
+    const handleVerticalClick = (value: string) => 
+    {
+      if (value === verticalActive) 
+      {
+        return;
+      }  
+      setVerticalActive(value);
+    }
 
 
     return (
-        <div>
-            <h1>Claims Placeholder</h1>
+        <div className='top-padding-30'>
+            <MDBRow  className='width-max'>
+                <MDBCol size='2'>
+                    <MDBTabs className='flex-column text-center'>
+                        <MDBTabsItem>
+                            <MDBTabsLink onClick={() => handleVerticalClick('tab1')} active={verticalActive === 'tab1'}>
+                                Active Claims
+                            </MDBTabsLink>
+                        </MDBTabsItem>
+                        <MDBTabsItem>
+                            <MDBTabsLink onClick={() => handleVerticalClick('tab2')} active={verticalActive === 'tab2'}>
+                                New Claim
+                            </MDBTabsLink>
+                        </MDBTabsItem>
+                        <MDBTabsItem>
+                            <MDBTabsLink onClick={() => handleVerticalClick('tab3')} active={verticalActive === 'tab3'}>
+                                Search
+                            </MDBTabsLink>
+                        </MDBTabsItem>
+                    </MDBTabs>
+                </MDBCol>
+                <MDBCol size='9'>
+                    <MDBTabsContent>
+                        <MDBTabsPane show={verticalActive === 'tab1'}>
+                            <h1>Claims Placeholder</h1>
+                            <p>
+                                search through claims? bring up info about the claim the files associated with the claim
+                                then be able to approve or deny the claim? 
+                                <p>
+                                    maybe this should have a table of all active claims the policy holder and vehicle associated with that claim?
+                                    <p>
+                                        adding a new claim should maybe be in the policy holders location?
+                                    </p>
+                                </p>                  
+                            </p>
+                        </MDBTabsPane>
+                        <MDBTabsPane show={verticalActive === 'tab2'}>
+                            Profile content
+                        </MDBTabsPane>
+                        <MDBTabsPane show={verticalActive === 'tab3'}>
+                            Messages content
+                        </MDBTabsPane>
+                    </MDBTabsContent>
+                </MDBCol>
+            </MDBRow>
         </div>       
     )    
 };
--- front-end/src/Components/Home.js
+++ front-end/src/Components/Home.js
@@ -1,15 +1,116 @@
-import React, { useState } from "react"
-
+import React from "react"
+import { MDBCarousel, MDBCarouselItem, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardImage, 
+    MDBCardBody, MDBCardTitle, MDBCardText, MDBCardFooter } from 'mdb-react-ui-kit';
 
 function Home ()
 {   
-
-
-
     return (
         <div>
-            <h1>Home Placeholder</h1>
-        </div>       
+            <div className='justify-content-center top-padding-30'>
+            <MDBContainer>
+                <MDBRow>
+                    <MDBCol size='sm' />
+                    <MDBCol size='md-10'>
+                        <MDBCarousel showControls fade className='shadowbox-sm'>
+                            <MDBCarouselItem
+                                className='w-100 d-block'
+                                itemId={1}
+                                src='https://mdbootstrap.com/img/new/slides/041.jpg'
+                                alt='...'>
+                                <div>
+                                    <h5 className='txt-shadow'>SomeCorp News</h5>
+                                    <p className="txt-shadow">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </div>                                
+                        </MDBCarouselItem>
+                            <MDBCarouselItem
+                                className='w-100 d-block'
+                                itemId={2}
+                                src='https://mdbootstrap.com/img/new/slides/042.jpg'
+                                alt='...'>
+                                    <div>
+                                        <h5 className='txt-shadow'>More SomeCorp News</h5>
+                                        <p className='txt-shadow'>Et labore reprehenderit anim labore consequat est adipisicing.</p>
+                                    </div>                                
+                            </MDBCarouselItem>
+
+                            <MDBCarouselItem
+                                className='w-100 d-block'
+                                itemId={3}
+                                src='https://mdbootstrap.com/img/new/slides/043.jpg'
+                                alt='...'>
+                                    <div>
+                                        <h5 className='txt-shadow'>Even More SomeCorp News</h5>
+                                        <p className='txt-shadow'>Ex consectetur anim magna ea velit velit.</p>
+                                    </div>                                
+                            </MDBCarouselItem>
+                        </MDBCarousel>
+                    </MDBCol>
+                    <MDBCol size='sm' />
+                </MDBRow>
+                <MDBRow className='row-cols-1 rows-cols-md-3 g-4 justify-content-center'>
+                    <MDBCol className='cards'>
+                        <MDBCard className='h-100'>
+                            <MDBCardImage
+                                src='https://mdbootstrap.com/img/new/standard/city/044.webp'
+                                alt='...'
+                                position='top' />
+                            <MDBCardBody>
+                                <MDBCardTitle>
+                                    Pariatur et culpa ea minim
+                                </MDBCardTitle>
+                                <MDBCardText>
+                                    Amet ullamco consequat nisi sunt nostrud sit adipisicing esse incididunt. 
+                                    Laboris consequat laborum aliquip nisi officia. Amet cillum ex aliqua.
+                                </MDBCardText>
+                            </MDBCardBody>
+                            <MDBCardFooter>
+                                <small className='text-muted'>Last updated 3 mins ago</small>
+                            </MDBCardFooter>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol className='cards'>
+                        <MDBCard className='h-100'>
+                            <MDBCardImage
+                                src='https://mdbootstrap.com/img/new/standard/city/043.webp'
+                                alt='...'
+                                position='top' />
+                            <MDBCardBody>
+                                <MDBCardTitle>
+                                    Incididunt non tempor
+                                </MDBCardTitle>
+                                <MDBCardText>
+                                    Lorem mollit Lorem et enim ullamco cillum. Pariatur enim duis ullamco fugiat id. 
+                                </MDBCardText>
+                            </MDBCardBody>
+                            <MDBCardFooter>
+                                <small className='text-muted'>Last updated 30 mins ago</small>
+                            </MDBCardFooter>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol className='cards'>
+                        <MDBCard className='h-100'>
+                            <MDBCardImage
+                                src='https://mdbootstrap.com/img/new/standard/city/042.webp'
+                                alt='...'
+                                position='top' />
+                            <MDBCardBody>
+                                <MDBCardTitle>
+                                    Dolor amet
+                                </MDBCardTitle>
+                                <MDBCardText>
+                                    Lorem mollit ut ut adipisicing laboris eiusmod sunt aute sit exercitation in deserunt elit. 
+                                    Do exercitation exercitation pariatur non consectetur non anim cillum ad esse. Voluptate sunt sit sint officia dolor ad eu et esse pariatur. 
+                                </MDBCardText>
+                            </MDBCardBody>
+                            <MDBCardFooter>
+                                <small className='text-muted'>Last updated 3 hours ago</small>
+                            </MDBCardFooter>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+            </div>            
+        </div>
     )    
 };
 
